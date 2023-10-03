f3451d2628b11fc5535a572fc743a438ee519661
f-pulido <fpulido@csumb.edu>
2023-10-03 00:57:56 -0700

    Created different views for various components of the site

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+    {
+        title: "PDF Editor",
+        subtitle: "Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://github.com/fernando-pulido/",
+    },
+    {
+        title: "Splasher Site",
+        subtitle: "Node.js and SQL",
+        description:
+            "A dyanmic social media site",
+        image: "./project-2.gif",
+        link: "https://github.com/fernando-pulido/Splasher-Site",
+    },
+    {
+        title: "Assignment Tracker",
+        subtitle: "Java",
+        description:
+            "Easy to use assignment tracker Android application.",
+        image: "./project-3.gif",
+        link: "https://github.com/f-pulido/AssignmentTracker",
+    },
+    {
+        title: "Panther Casino",
+        subtitle: "C++",
+        description:
+            "Entertaining variety of casino games and services.",
+        image: "./project-4.gif",
+        link: "https://github.com/fernando-pulido/CSS2A-Final-Group-Project",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jorge Pulido",
+        company: "Gators Inc.",
+    },
+    {
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/94.jpg",
+        name: "Ruben Alvarez",
+        company: "Dooble.io",
+    }
+];
+
+export const skills = [
+    "Python",
+    "C++",
+    "Java",
+    "JavaScript",
+    "Node",
+    "SQL",
+    "HTML",
+    "CSS"
+];
